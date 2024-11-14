3328b2f1c2e8dbed89a0f8630c8cde417032a698
Shaymaa2006 <shaymaa200618@gmail.com>
2024-11-14 12:38:07 -0500

    time

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Hello!');
+const todaysDate = () => {
+    let d = new Date();
+    return "Todays date is " + d;
+}
+const dateBlock =document.getElementById("dateblock")
+dateBlock.innerText = todaysDate();
+
+const body= document.body
+const d = new Date();
+
+if (d.getHours() > 20 || d.getHours() <8) {
+    body.style["backgroundColor"] ="black";
+    body.style["backgroundColor"] ="White";
+} else {
+    body.style["backgroundColor"] ="White";
+    body.style["color"]= "black";
+}
