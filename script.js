74ae97bfbdfae9198fc935dead6baffbd5efe0b3
Shaymaa2006 <shaymaa200618@gmail.com>
2024-12-12 12:39:40 -0500

    december 12

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ dateBlock.innerText = todaysDate();
 const body= document.body
 const d = new Date();
 
-if (d.getHours() > 20 || d.getHours() <8) {
-    body.style["backgroundColor"] ="black";
-    body.style["backgroundColor"] ="White";
-} else {
-    body.style["backgroundColor"] ="White";
-    body.style["color"]= "black";
-}
+// if (d.getHours() > 20 || d.getHours() <8) {
+//     body.style["backgroundColor"] ="black";
+//     body.style["backgroundColor"] ="White";
+// } else {
+//     body.style["backgroundColor"] ="White";
+//     body.style["color"]= "black";
+// }
